c8454e6a43a44d95558c7a713c3bd40ac3895532
JoAnn Siciliano <joannsiciliano@gmail.com>
2021-02-20 19:18:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,7 @@ def long_planeteer_calls(planeteer_calls)
   long_planeteer_calls.all? {|call| call.length<4}
 end
 long_planeteer_calls(planeteer_calls)
+
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
