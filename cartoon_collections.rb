86462b90406edb1f7f2a4378c22b4e75bc262f30
JoAnn Siciliano <joannsiciliano@gmail.com>
2021-02-20 18:54:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def square_array(array)
   new_array=[]
   index = 0 
   while index < array.length do
-    new array << array[idnex] * array [index]
+    new array << array[index] * array [index]
     index += 1 
   end
   new_array
